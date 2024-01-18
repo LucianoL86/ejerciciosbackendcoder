ae7684922ea9beddc70c161e55435a9f65e8f1d6
Luciano Lanza <lucianolanza@hotmail.com>
2024-01-18 01:56:11 -0300

    creación de carpeta config

--- ejercicio05/src/index.js
+++ ejercicio05/src/index.js
@@ -1,10 +1,10 @@
 import realTimeServer from './realTimeServer.js'
 import app from './app.js'
 import dotenv from 'dotenv'
+import { PORT } from './config/index.config.js'
 
 dotenv.config()
 
-const PORT = process.env.PORT || 8080
 
 const httpServer = app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
