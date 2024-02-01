d891e6dc334b1e2cc1a3ef85678eae3f98a92697
Luciano Lanza <lucianolanza@hotmail.com>
2024-02-01 20:49:21 -0300

    Implementación de login

--- ejercicio06/src/DAOs/mongodb/models/users.model.js
+++ ejercicio06/src/DAOs/mongodb/models/users.model.js
@@ -15,7 +15,8 @@ const userSchema = new mongoose.Schema({
     email: {
         type: String,
         required: true,
-        max: 100
+        max: 100,
+        unique: true
     },
     password: {
         type: String,
