834e1fbd890f6d88bc645bd0089adc372cb22027
Luciano Lanza <lucianolanza@hotmail.com>
2024-02-14 03:38:10 -0300

    Refactor al login

--- ejercicio07/src/controllers/session.controller.js
+++ ejercicio07/src/controllers/session.controller.js
@@ -1,76 +1,84 @@
-import { Router } from "express";
-import userModel from "../DAOs/mongodb/models/users.model.js";
-import { auth } from "../middleware/index.js";
+import { Router } from 'express'
+import userModel from '../DAOs/mongodb/models/users.model.js'
+import { auth } from '../middleware/index.js'
+import { comparePassword, getHashedPassword } from '../utils.js'
 
 const router = Router();
 
 router.post("/login", async (req, res) => {
-    const { email, password } = req.body;
+    try {
 
-    if (email === 'adminCoder@coder.com' && password === 'adminCod3r123') {
-        req.session.user = email;
-        req.session.role = "admin";
-        res.status(200).json({
-            respuesta: "ok",
-        });
-    } else {
-        const result = await userModel.findOne({ email, password });
+        const { email, password } = req.body;
 
-        if (result === null) {
+        if (email === 'adminCoder@coder.com' && password === 'adminCod3r123') {
+            req.session.user = email;
+            req.session.role = "admin";
+            res.status(200).json({
+                respuesta: "ok",
+            });
+        } else {
+            const user = await userModel.findOne({ email });
+
+            if (user === null || !comparePassword(password, user.password)) {
+                res.status(400).json({
+                    error: "Usuario o contraseña incorrectos",
+                });
+            } else {
+                req.session.user = {
+                    email: user.email,
+                    role: user.role,
+                }
+                
+                const { first_name, last_name } = user;
+                res.status(200).json({
+                    respuesta: "ok",
+                    user: { first_name, last_name },
+                });
+            }
+        }
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            error: "Error al iniciar sesión",
+        })
+    }
+})
+
+
+router.post("/signup", async (req, res) => {
+    try {
+        const { first_name, last_name, email, password, age } = req.body
+        const existingUser = await userModel.findOne({ email });
+
+        if (existingUser) {
             res.status(400).json({
-                error: "Usuario o contraseña incorrectos",
+                error: "El usuario ya existe",
+            });
+            return;
+        }
+
+        const newUser = await userModel.create({
+            first_name,
+            last_name,
+            age,
+            email,
+            password: getHashedPassword(password)
+        });
+
+        if (newUser === null) {
+            res.status(400).json({
+                error: "Error al crear el usuario",
             });
         } else {
             req.session.user = email;
-            req.session.role = "user";
-
-            const { first_name, last_name } = result;
-            res.status(200).json({
-                respuesta: "ok",
-                user: { first_name, last_name },
+            req.session.role = "admin";
+            res.status(201).json({
+                respuesta: "Usuario creado con éxito",
             });
         }
-    }
-});
-router.post("/signup", async (req, res) => {
-    console.log(req.body);
-    const { first_name, last_name, email, password, age } = req.body
-    const existingUser = await userModel.findOne({ email });
-
-    if (existingUser) {
-        res.status(400).json({
-            error: "El usuario ya existe",
-        });
-        return;
-    }
-
-    const newUser = {
-        first_name,
-        last_name,
-        email,
-        password,
-        age,
-        role: "user",
-    };
-    console.log(email);
-
-    const result = await userModel.create({
-        first_name,
-        last_name,
-        age,
-        email,
-        password,
-    });
-
-    if (result === null) {
-        res.status(400).json({
-            error: "Error al crear el usuario",
-        });
-    } else {
-        req.session.user = email;
-        req.session.role = "admin";
-        res.status(201).json({
-            respuesta: "Usuario creado con éxito",
+    } catch {
+        res.status(500).json({
+            error: "Error al registrar usuario",
         });
     }
 });
