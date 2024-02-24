4eb76319e57fd89edbe910d34402bb9f3520c9f1
Luciano Lanza <lucianolanza@hotmail.com>
2024-02-24 13:08:49 -0300

    Refactor al login

--- ejercicio07/src/controllers/views.controller.js
+++ ejercicio07/src/controllers/views.controller.js
@@ -1,17 +1,16 @@
 import { Router } from 'express'
 import isAuthenticated from '../middleware/isAuthenticated.js'
-import { authenticate } from 'passport'
 
 const router = Router()
 
-router.get('/signup', authenticate, (req, res) => {
+router.get('/signup', isAuthenticated, (req, res) => {
     res.render('signup', {
         title: 'Registrarse',
         style: 'users.css'
     })
 })
 
-router.get('/login', authenticate,(req, res) => {
+router.get('/login', isAuthenticated,(req, res) => {
     res.render('login', {
         title: 'Iniciar Sesión',
         style: 'users.css'
